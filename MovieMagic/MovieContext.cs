474a9854e57465db5359203fe192326600b5fdfc
Matthew.Reynolds <mattreynolds84@outlook.com>
2017-08-09 13:50:02 -0400

    Add project files.

---
+++ MovieMagic/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieMagic.Models;
+
+namespace MovieMagic
+{
+    public class MovieContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
---
+++ MovieMagic/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieMagic.Models;
+
+namespace MovieMagic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+
+        private static void ReadArgs(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "hi":
+                        Console.WriteLine("Hello World!");
+                        break;
+                    case "foo":
+                        Console.WriteLine("bar");
+                        break;
+                    default:
+                        Console.WriteLine($"{args[0]} is not a valid command, please try again");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unable to process your request.");
+            }
+        }
+
+        private static void Test()
+        {
+            var ctx = new MovieContext();
+            ctx.Movies.Add(new Movie(){Title = "Test Title"});
+            ctx.SaveChanges();
+
+
+            var movies = ctx.Movies.ToList();
+            foreach (var movie in movies)
+            {
+                Console.WriteLine($"{movie.Id}: {movie.Title}");
+            }
+        }
+    }
+}
